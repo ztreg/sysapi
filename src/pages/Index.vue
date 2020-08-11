62acc538cb147a009eae98b41f84799f7e0ce05d
Jonas Gertz <Geertzo@live.se>
2020-08-11 20:30:49 +0200

    fetching 10 products in table

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="row">
+      <q-table
+      :data="this.products"
+      row-key="name"
+      class="col-12"
+      rows-per-page-label = "10"
+    />
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      products: []
+    }
+  },
+  async mounted () {
+    await fetch('https://bolaget.io/v1/products')
+      .then(response => response.json())
+      .then((response) => {
+        this.products = response
+        console.log(this.products)
+      })
+      .catch((error) => {
+        console.error('There was a error fetching:', error)
+      })
+  }
 }
 </script>
